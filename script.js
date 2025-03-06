e4eac5a03ffa5e84d80d74a8d27a6623623e902c
Parikshit Mali <parikshitmali17@gmail.com>
2025-03-06 20:25:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+let submit=document.querySelector("#submit");
+let ExistingLogInBtn=document.querySelector("#existing");
+let rememberCheckbox=document.getElementById("checkbox");
+let username=document.querySelector("#username").value;
+let password =document.getElementById("password");
+let isSaved=false;
+
+submit.addEventListiner('click',()=>{
+	alert(`Logged in as ${username}`)
+	if(rememberCheckbox){
+		localStorage.setItem('user',JSON.Stringfy({user: username, pass:password}))
+	isSaved=true;
+		alert(`Logged in as ${username}`)
+	}else{
+		localStorage.clear('')	
+	}
+
+	(isSaved) ? ExistingLogInBtn.style.display=='none' :null
+	
+})
+
+
